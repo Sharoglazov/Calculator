3311d1aacd214a090806c9fb975eb1b686e0d210
Egor_Sharoglazov <e.sharoglazov@mai.ru>
2023-10-16 12:54:24 +0500

    adding operation exponentiation

--- begin.cpp
+++ begin.cpp
@@ -1,26 +1,38 @@
 #include <iostream>
 
 int main() {
-    std::cout<<"Please, enter the first number"<<std::endl;
+    std::cout << "Please, enter the first number" << std::endl;
     int a;
-    std::cin>>a;
-    std::cout<<"Please, enter the second number"<<std::endl;
+    std::cin >> a;
+    std::cout << "Please, enter the second number" << std::endl;
     int b;
-    std::cin>>b;
-    std::cout<<"Please, select the type of operation from list: '+', '-', '*', '/'"<<std::endl;
+    std::cin >> b;
+    std::cout << "Please, select the type of operation from list: '+', '-', '*', '/', '^' " << std::endl;
     char t;
-    std::cin>>t;
-    double c;
+    std::cin >> t;
+    int c;
     switch (t) {
-        case '+':
-            std::cout<<a+b<<std::endl;
-        case '-':
-            std::cout<<a-b<<std::endl;
-        case '*':
-            std::cout<<a*b<<std::endl;
-        case '/':
-            c = (double)a / (double)b;
-            std::cout<<c<<std::endl;
+    case '+':
+        std::cout << a + b << std::endl;
+        break;
+    case '-':
+        std::cout << a - b << std::endl;
+        break;
+    case '*':
+        std::cout << a * b << std::endl;
+        break;
+    case '/':
+        double c;
+        c = (double)a / (double)b;
+        std::cout << c << std::endl;
+        break;
+    case '^':
+        c = a;
+        for (int i = 1; i < b; i++) {
+            c *= a;
+        }
+        std::cout << c << std::endl;
+        break;
     }
 
     return 0;
