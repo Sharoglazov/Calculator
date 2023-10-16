e51a6ce385a156c01c282ca2db23d1819e74478b
Egor_Sharoglazov <e.sharoglazov@mai.ru>
2023-10-16 10:37:46 +0500

    adding 4 operations

--- begin.cpp
+++ begin.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 
 int main() {
-    std::cout<<"Hello"<<std::endl;
+    std::cout<<"Please, enter the first number"<<std::endl;
+    int a;
+    std::cin>>a;
+    std::cout<<"Please, enter the second number"<<std::endl;
+    int b;
+    std::cin>>b;
+    std::cout<<"Please, select the type of operation from list: '+', '-', '*', '/'"<<std::endl;
+    char t;
+    std::cin>>t;
+    double c;
+    switch (t) {
+        case '+':
+            std::cout<<a+b<<std::endl;
+        case '-':
+            std::cout<<a-b<<std::endl;
+        case '*':
+            std::cout<<a*b<<std::endl;
+        case '/':
+            c = (double)a / (double)b;
+            std::cout<<c<<std::endl;
+    }
+
     return 0;
 }
